7c47bb5ed673e46c0f600dc7aa8d1183ca842058
elektromedisiwip <elektromedisiwip@gmail.com>
2025-02-01 09:38:23 +0900

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRaN-pGdkrZSy0CwX3kgO6V5kX-xKPzP8",
+    authDomain: "website-1d779.firebaseapp.com",
+    databaseURL: "https://website-1d779-default-rtdb.firebaseio.com",
+    projectId: "website-1d779",
+    storageBucket: "website-1d779.firebasestorage.app",
+    messagingSenderId: "451587827423",
+    appId: "1:451587827423:web:10d6f707dedff3ae26ab6a",
+    measurementId: "G-LNE2ZV9FXZ"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
