9820e00fbcff4779c80a190cc8eee685d41bc696
elektromedisiwip <elektromedisiwip@gmail.com>
2025-02-04 12:14:44 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,43 @@
-const login = ()=> {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential)=> {
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRaN-pGdkrZSy0CwX3kgO6V5kX-xKPzP8",
+    authDomain: "website-1d779.firebaseapp.com",
+    databaseURL: "https://website-1d779-default-rtdb.firebaseio.com",
+    projectId: "website-1d779",
+    storageBucket: "website-1d779.firebasestorage.app",
+    messagingSenderId: "451587827423",
+    appId: "1:451587827423:web:10d6f707dedff3ae26ab6a",
+    measurementId: "G-LNE2ZV9FXZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const login = document.getElementById('btn-login');
+login.addEventListener("click",function(event) {
+  event.preventDefault()
+
+  //input
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if(email && password) {
+    signInWithEmailAndPassword(auth,email,password)
+      .then ((userCredential) => {
         const user = userCredential.user;
-        console.log('login berhasil');
-        window.location.href ='dashboard.html';
-    })
-    .catch((error)=> {
-        console.error('login gagal:', error.message);
-        alert('login gagal!');
-    });
-};
+        alert("berhasil login")
+        window.location.href = "dashboard.html";
 
-const logout =()=> {
-    firebase.auth().signOut()
-    .then ( ()=> {
-        console.log('logout Berhasil');
-        window.location.href ='index.html';
-    })
-    .catch((error) => {
-        console.error('logout gagal:', error.message);
-        alert('logout gagal!');
-    });
-};
-
-const cekLogin = (callback) => {
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            callback(true);
-        } else {
-            callback(false);
-        }
-    });
-};
-
-const arahkanKeLogin = () => {
-    cekLogin((isLoggedIn) => {
-        if (!isLoggedIn) {
-            window.location.href = 'index.html';
-        }
-    });
-};
-
-document.getElementById ('btn-login').addEventListener('click',login);
-document.getElementById ('Sign_Out').addEventListener('click',logout);
-
-document.addEventListener('DOMContentLoaded',
-    function(){
-        arahkanKeLogin();
-    });
-
-    document.addEventListener('DOMContentLoaded',
-        function(){
-            cekLogin((isLoggedIn) => {
-                if (!isLoggedIn) {
-                    window.location.href = 'index.html';
-                }
-            });
-        });
+      })
+      .catch ((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage)
+      });
+  } else {
+    alert("Silakan isi email dan password")
+  }
+})
